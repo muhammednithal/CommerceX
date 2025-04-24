08ae79f8c6aee26e7733f425fbc3d39de9557710
muhammednithal <muhammednithalb14@gmail.com>
2025-04-24 08:35:30 +0530

    feat: add swagger for api doc

--- commerce-x-backend/src/main.ts
+++ commerce-x-backend/src/main.ts
@@ -1,8 +1,22 @@
+// main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('CommerceX API')
+    .setDescription('E-commerce backend API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger available at /api
+
+  await app.listen(3000);
 }
 bootstrap();
